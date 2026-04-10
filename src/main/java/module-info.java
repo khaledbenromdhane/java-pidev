fc42f8cf6c7eda6cb23ff0acacfb0cccbc2805df
khaledbenromdhane <khaled.benromdhane@esprit.tn>
2026-04-10 21:06:57 +0100

    fix module-info for non-fxml run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,9 @@
 module com.pidev.pidev {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.web;
     requires java.sql;
 
 
-    opens com.pidev to javafx.fxml;
-    opens com.pidev.controllers to javafx.fxml;
     exports com.pidev;
     exports com.pidev.entities;
 }
